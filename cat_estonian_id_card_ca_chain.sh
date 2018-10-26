5101f71e089c8b56a5013bac473d11bac6b24e7f
Siim Puustusmaa <siimpuustusmaa@gmail.com>
2018-10-26 13:17:00 +0300

    esteid2018

--- cat_estonian_id_card_ca_chain.sh
+++ cat_estonian_id_card_ca_chain.sh
@@ -5,8 +5,12 @@ wget https://www.sk.ee/upload/files/ESTEID-SK_2011.pem.crt
 wget https://www.sk.ee/upload/files/ESTEID-SK_2015.pem.crt
 wget https://www.sk.ee/upload/files/EID-SK_2011.pem.crt
 wget https://www.sk.ee/upload/files/EID-SK_2016.pem.crt
+wget http://c.sk.ee/EE-GovCA2018.pem.crt
+wget http://c.sk.ee/esteid2018.pem.crt
+
 cat 'EE_Certification_Centre_Root_CA.pem.crt' 'ESTEID-SK_2011.pem.crt' 'ESTEID-SK_2015.pem.crt' \
-'EID-SK_2011.pem.crt' 'EID-SK_2016.pem.crt' > id.crt
+'EID-SK_2011.pem.crt' 'EID-SK_2016.pem.crt' 'EE-GovCA2018.pem.crt' 'esteid2018.pem.crt' > id.crt
+
 rm 'EE_Certification_Centre_Root_CA.pem.crt' 'ESTEID-SK_2011.pem.crt' 'ESTEID-SK_2015.pem.crt' \
-'EID-SK_2011.pem.crt' 'EID-SK_2016.pem.crt'
+'EID-SK_2011.pem.crt' 'EID-SK_2016.pem.crt' 'EE-GovCA2018.pem.crt' 'esteid2018.pem.crt'
 
