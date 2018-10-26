40c72372164d578413d03c48f7d95c256934a8a7
Siim Puustusmaa <siimpuustusmaa@gmail.com>
2018-10-26 15:52:25 +0300

    typo

--- cat_estonian_id_card_ca_chain.sh
+++ cat_estonian_id_card_ca_chain.sh
@@ -25,7 +25,7 @@ wget https://sk.ee/upload/files/TEST_of_EE-GovCA2018.pem.crt
 wget https://sk.ee/upload/files/TEST_of_ESTEID2018.pem.crt
 
 cat id.crt TEST_of_EE_Certification_Centre_Root_CA.pem.crt TEST_of_ESTEID-SK_2011.pem.crt TEST_of_ESTEID-SK_2015.pem.crt \
-TEST_of_EID-SK_2011.pem.crt TEST_of_EID-SK_2016.pem.crt TEST_of_EE-GovCA2018.pem.crt TEST_of_ESTEID2018.pem.crt test-id.crt
+TEST_of_EID-SK_2011.pem.crt TEST_of_EID-SK_2016.pem.crt TEST_of_EE-GovCA2018.pem.crt TEST_of_ESTEID2018.pem.crt > test-id.crt
 
 rm TEST_of_EE_Certification_Centre_Root_CA.pem.crt TEST_of_ESTEID-SK_2011.pem.crt TEST_of_ESTEID-SK_2015.pem.crt \
 TEST_of_EID-SK_2011.pem.crt TEST_of_EID-SK_2016.pem.crt TEST_of_EE-GovCA2018.pem.crt TEST_of_ESTEID2018.pem.crt
