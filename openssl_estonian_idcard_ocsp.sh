98a5aab3024bc1fc5008c60c721091918a39db41
Siim Puustusmaa <siimpuustusmaa@gmail.com>
2018-11-30 16:03:32 +0200

    added ocsp check from SK demo ocsp

--- openssl_estonian_idcard_ocsp.sh
+++ openssl_estonian_idcard_ocsp.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
-openssl ocsp -issuer ESTEID-SK_2015.pem.crt -cert idcard.crt -VAfile SK_OCSP_RESPONDER_2011.pem.cer -url http://ocsp.sk.ee
+openssl ocsp -issuer ESTEID-SK_2015.pem.crt -cert idcard_authentication.crt -VAfile SK_OCSP_RESPONDER_2011.pem.cer -url http://ocsp.sk.ee
 
+# in OCSP test environment (https://demo.sk.ee/upload_cert/index.php)
+# openssl ocsp -issuer ESTEID-SK_2015.pem.crt -cert idcard_authentication.crt -VAfile TEST_of_SK_OCSP_RESPONDER_2011.pem.cer -url http://demo.sk.ee/ocsp
