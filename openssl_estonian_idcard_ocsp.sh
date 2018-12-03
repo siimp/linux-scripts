7cc7e302d3ea92977195ba34caee41904df104e7
Siim Puustusmaa <siimpuustusmaa@gmail.com>
2018-12-03 13:06:54 +0200

    more accurate ldap base

--- openssl_estonian_idcard_ocsp.sh
+++ openssl_estonian_idcard_ocsp.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
 # getting public certificates via LDAP
-# ldapsearch -x -h ldap.sk.ee -b c=EE "(serialNumber=<PERSON_ID_CODE_HERE>)"
+# ldapsearch -x -h ldap.sk.ee -b "ou=authentication,o=ESTEID,c=EE" "(serialNumber=<PERSON_ID_CODE_HERE>)"
 # new ESTEID 2018 ldap service
-# ldapsearch -x -H ldaps://esteid.ldap.sk.ee -b c=EE "(serialNumber=PNOEE-<PERSON_ID_CODE_HERE>)"
+# ldapsearch -x -H ldaps://esteid.ldap.sk.ee -b "ou=Authentication,o=Identity card of Estonian citizen,dc=ESTEID,c=EE" "(serialNumber=PNOEE-<PERSON_ID_CODE_HERE>)"
 
 openssl ocsp -issuer ESTEID-SK_2015.pem.crt -cert idcard_authentication.crt -VAfile SK_OCSP_RESPONDER_2011.pem.cer -url http://ocsp.sk.ee
 
