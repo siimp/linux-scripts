e435e4688fc4e886360057545f26211969c1b0c4
Siim Puustusmaa <siimpuustusmaa@gmail.com>
2021-04-20 12:47:58 +0300

    keypair check

--- openssl_generate_self-signed_cert.sh
+++ openssl_generate_self-signed_cert.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env sh
+# to validate RSA keypairs diff <(openssl x509 -in cert.pem -noout -modulus) <(openssl rsa -in key.pem -noout -modulus)
 openssl req -x509 -sha256 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365 -nodes -subj '/CN=*.localhost'
